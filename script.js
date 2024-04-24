ff4d06ea0560ab3099825d1442b51dc744ac9bc9
julsCadenas <jscadenas12@gmail.com>
2024-04-24 15:23:27 +0800

    added compute gwa function

--- script.js
+++ script.js
@@ -6,26 +6,46 @@ $(document).ready(function() {
         subjectCount++;
 
         var newSubjectDiv = $("<div class='inputRow'><div class='subjects'><h4>Subject " + subjectCount + "</h4></div></div>");
-        var newGradeBox = $("<div class='gradeBoxes'><input type='number' placeholder='Final Grade' class='grade'></div>");
+        var newGradeBox = $("<div class='gradeBoxes'><input type='number' placeholder='Grade' class='grade'></div>");
         var newUnitBox = $("<div class='unitBoxes'><input type='number' placeholder='Units' class='unit'></div>");
 
         $(".inputContainer").append(newSubjectDiv);
         newSubjectDiv.append(newGradeBox);
         newSubjectDiv.append(newUnitBox);
+        
     });
-});
 
 /*
-
     formula ng gwa with units
-
-    (Summation ng Grade x Units) / (Total Units)
-
-    Kunyari
-        4   -   3 units
-        3.5 -   1 unit
+    (Grade x Units) / (Total Units)
+    Example:
+        4   :   3 units
+        3.5 :   1 unit
 
     (4*3 + 3.5*1)/4
-
 */
 
+    $(".computeBtn").click(function() {
+        var totalGradeUnits = 0;
+        var totalUnits = 0;
+
+        $(".inputRow").each(function() {
+            var grade = parseFloat($(this).find(".grade").val());
+            var units = parseFloat($(this).find(".unit").val());
+
+            if (!isNaN(grade) && !isNaN(units)) {
+                totalGradeUnits += grade * units;
+                totalUnits += units;
+            }
+        });
+
+        if (totalUnits !== 0) {
+            var gwa = totalGradeUnits / totalUnits;
+            // alert("Your GWA is: " + gwa.toFixed(4));
+            $(".gwaResult").html("<h2 class='result'>Your GWA is: " + gwa.toFixed(4) + "</h2>");
+        } else {
+            alert("Please enter valid grades and units.");
+        }
+    });
+
+});
