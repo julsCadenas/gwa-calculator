e6251d93b80eaa9330f42c9e4122c81a221de50c
julsCadenas <jscadenas12@gmail.com>
2024-04-24 14:14:54 +0800

    added textbox append

--- script.js
+++ script.js
@@ -1,49 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the "Add Subject" button
-    var addButton = document.querySelector('.subjBtn');
-    // Get the container where new subject inputs will be added
-    var subjectContainer = document.getElementById('inputContainer');
+// code to add new subjects and textboxes
+$(document).ready(function() {
+    var subjectCount = 1; 
 
-    // Counter for keeping track of the number of subjects
-    var subjectCount = 1;
-
-    // Event listener for the "Add Subject" button
-    addButton.addEventListener('click', function() {
-        // Increment the subject count
+    $(".subjBtn").click(function() {
         subjectCount++;
 
-        // Create new elements for the new subject input
-        var newSubject = document.createElement('div');
-        newSubject.classList.add('subject');
+        var newSubjectDiv = $("<div class='subjects'><h4>Subject " + subjectCount + "</h4></div>");
+        var newGradeBox = $("<div class='gradeBoxes'><input type='number' placeholder='Final Grade' class='grade'></div>");
+        var newUnitBox = $("<div class='unitBoxes'><input type='number' placeholder='Units' class='unit'></div>");
 
-        var subjectsDiv = document.createElement('div');
-        subjectsDiv.classList.add('subjects');
-        var subjectHeading = document.createElement('h3');
-        subjectHeading.textContent = 'Subject ' + subjectCount;
-        subjectsDiv.appendChild(subjectHeading);
-
-        var gradeBoxesDiv = document.createElement('div');
-        gradeBoxesDiv.classList.add('gradeBoxes');
-        var gradeInput = document.createElement('input');
-        gradeInput.type = 'number';
-        gradeInput.placeholder = 'Number of Units';
-        gradeInput.classList.add('grade');
-        gradeBoxesDiv.appendChild(gradeInput);
-
-        var unitBoxesDiv = document.createElement('div');
-        unitBoxesDiv.classList.add('unitBoxes');
-        var unitInput = document.createElement('input');
-        unitInput.type = 'number';
-        unitInput.placeholder = 'Final Grade';
-        unitInput.classList.add('unit');
-        unitBoxesDiv.appendChild(unitInput);
-
-        // Append new elements to the new subject container
-        newSubject.appendChild(subjectsDiv);
-        newSubject.appendChild(gradeBoxesDiv);
-        newSubject.appendChild(unitBoxesDiv);
-
-        // Append the new subject container to the subject container
-        subjectContainer.appendChild(newSubject);
+        $(".inputContainer").append(newSubjectDiv);
+        $(".inputContainer").append(newGradeBox);
+        $(".inputContainer").append(newUnitBox);
     });
 });
+
