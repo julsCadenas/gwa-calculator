ebe37fc4105cb2dc90159f7fe8b3bbc90b569182
julsCadenas <jscadenas12@gmail.com>
2024-04-24 14:48:42 +0800

    fixed jquery append

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ $(document).ready(function() {
     $(".subjBtn").click(function() {
         subjectCount++;
 
-        var newSubjectDiv = $("<div class='subjects'><h4>Subject " + subjectCount + "</h4></div>");
+        var newSubjectDiv = $("<div class='inputRow'><div class='subjects'><h4>Subject " + subjectCount + "</h4></div></div>");
         var newGradeBox = $("<div class='gradeBoxes'><input type='number' placeholder='Final Grade' class='grade'></div>");
         var newUnitBox = $("<div class='unitBoxes'><input type='number' placeholder='Units' class='unit'></div>");
 
         $(".inputContainer").append(newSubjectDiv);
-        $(".inputContainer").append(newGradeBox);
-        $(".inputContainer").append(newUnitBox);
+        newSubjectDiv.append(newGradeBox);
+        newSubjectDiv.append(newUnitBox);
     });
 });
+
+/*
+
+    formula ng gwa with units
+
+    (Summation ng Grade x Units) / (Total Units)
+
+    Kunyari
+        4   -   3 units
+        3.5 -   1 unit
+
+    (4*3 + 3.5*1)/4
+
+*/
 
